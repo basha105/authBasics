a9d038fed5ac766b087feadaee3ace072f080acc
basha105 <brentbasha17@gmail.com>
2025-06-10 14:56:57 -0700

    passport

--- app.js
+++ app.js
@@ -28,6 +28,26 @@ app.post("/sign-up", async (req, res, next) => {
         return next(err);
     }
 });
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+            const user = rows[0];
+
+            if (!user) {
+                return done(null, false, { message: "Incorrect username" });
+            }
+            if (user.password !== password) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+            return done(null, user);
+        } catch(err) {
+            return done(err);
+        }
+    })
+);
+
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
 app.get("/", (req, res) => res.render("index"));
 app.listen(3000, () => console.log("app listening on port 3000"));
